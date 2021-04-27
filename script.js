d8d8fca6bfe117c8bdab2ca045dc9e3d68dee027
Yilun Xiao <yilunxiao2@gmail.com>
2021-04-27 16:57:30 -0400

    tried different paths

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 // Get data from NYT best sellers API
 var nytBase = "https://api.nytimes.com/svc/books/v3";
 var nytPath = "/reviews.json?author=Stephen+King&api-key=";
-var nytKey = "W9jAKsLKDW0evuFLFufiJsZCcXL7A2F9";
-var nytURL = nytBase + nytPath + nytKey;
+var nytPath1 = "/lists/names.json?";
+var nytPath2 = "/lists/best-sellers/history.json?";
+var nytPath3 = "/lists/overview.json?";
+var nytKey = "api-key=tZmSouJCKxYwB50PAcr0v6vFs6EI8yNm";
+var nytSecret = "OeaEEqlPYBWtKSxa"
 function getNYT() {
-    fetch(nytURL)
+    var nytURL = nytBase + nytPath3 + nytKey;
+    fetch(nytURL, {
+        method: "GET",
+        headers: {
+            "Accept": "application/json"
+        },
+    })
         .then(function (response) {
             return response.json();
         })
