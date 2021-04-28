b98def4da52b6912881c8b581a3c7b0d4dc465f7
Yilun Xiao <yilunxiao2@gmail.com>
2021-04-28 11:32:15 -0400

    added book image source from NYT api

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // DEPENDENCIES
-var book = $('#bookImg');
+var book = $('.title-image');
 
 // Get data from NYT best sellers API
 var nytBase = "https://api.nytimes.com/svc/books/v3";
-// var nytPath = "/reviews.json?author=Stephen+King&api-key=";
 // var nytPath = "/lists/names.json?bestsellers-date=2009-04-28&";
-var nytPath = "/lists/best-sellers/history.json?title=Change of Heart&";
+// var nytPath = "/lists/best-sellers/history.json?title=Change of Heart&";
 // var nytPath = "/lists/overview.json?";
-// var nytPath = "/lists/2019-01-20/hardcover-fiction.json?";
+var nytPath = "/lists/2019-01-20/hardcover-fiction.json?";
 var nytKey = "api-key=tZmSouJCKxYwB50PAcr0v6vFs6EI8yNm";
 var nytSecret = "OeaEEqlPYBWtKSxa"
 function getNYT() {
@@ -23,8 +22,8 @@ function getNYT() {
         })
         .then(function (data) {
             console.log(data);
-            // bookImg = data.results.books[0].book_image;
-            // book.prepend('<img id="theImg" src="' + bookImg + '" />')
+            bookImg = data.results.books[0].book_image;
+            book.attr("src", bookImg)
         });
 }
 
