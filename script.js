600356b2ddbe668143277a8aa6045a026fd434ee
Yilun Xiao <yilunxiao2@gmail.com>
2021-04-28 11:34:33 -0400

    commented in functions

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ var book = $('.title-image');
 // Get data from NYT best sellers API
 var nytBase = "https://api.nytimes.com/svc/books/v3";
 // var nytPath = "/lists/names.json?bestsellers-date=2009-04-28&";
+// Searches for book with specific title
 // var nytPath = "/lists/best-sellers/history.json?title=Change of Heart&";
-// var nytPath = "/lists/overview.json?";
+// Gets list from specified date and section, includes image
 var nytPath = "/lists/2019-01-20/hardcover-fiction.json?";
 var nytKey = "api-key=tZmSouJCKxYwB50PAcr0v6vFs6EI8yNm";
 var nytSecret = "OeaEEqlPYBWtKSxa"
@@ -27,5 +28,5 @@ function getNYT() {
         });
 }
 
-
+// Runs function to get data from NYT api
 getNYT();
